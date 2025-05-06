b74d8f6ad7ff3264951229856a8ecb7f2850078f
Home <calle.carolina@gmail.com>
2025-05-06 23:14:07 +0200

    refactor(main): integrate ChakraProvider and custom theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
+import { system } from "./theme/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider value={system}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
