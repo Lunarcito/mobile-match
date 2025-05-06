fd9af5759639fef07657f49e5a50ec7040c45de3
Home <calle.carolina@gmail.com>
2025-05-06 23:47:36 +0200

    feat(home): add search input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Input, Heading } from "@chakra-ui/react";
 
 function Home() {
-  return <h2>Bienvenido</h2>;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <Box p={3}>
+      <Heading mb={3}>Product List</Heading>
+      <Input
+        placeholder="Search products"
+        mb={3}
+        maxW="300px"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </Box>
+  );
 }
 
 export default Home;
