cfe99ed03f757de7f057c86592421d625fa9f43a
Home <calle.carolina@gmail.com>
2025-05-07 13:06:18 +0200

    refactor: consolidate routing into App.jsx and remove AppRoutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import AppRoutes from "./routes/AppRoutes";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import { ChakraProvider } from "@chakra-ui/react";
+import { system } from "./theme/theme.js";
 
 function App() {
   return (
-    <div>
+    <ChakraProvider value={system}>
       <h1>Mobile Tech</h1>
-      <AppRoutes />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </ChakraProvider>
   );
 }
 
