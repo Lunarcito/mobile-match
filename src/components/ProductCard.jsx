c42952da0697bb3b2d1cfeea2aaa1e217355a19e
Home <calle.carolina@gmail.com>
2025-05-07 20:30:49 +0200

    feat: update ProductCard to display brand, model, image and price

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Image, VStack } from "@chakra-ui/react";
 
 const ProductCard = ({ product, onClick }) => {
   return (
@@ -8,11 +8,20 @@ const ProductCard = ({ product, onClick }) => {
       borderWidth="1px"
       borderRadius="md"
       textAlign="center"
+      overflow="hidden"
       cursor="pointer"
       _hover={{ shadow: "md" }}
       onClick={() => onClick(product.id)}
     >
-      <Text fontWeight="bold">{product.name}</Text>
+      <VStack spacing={3}>
+        <Image src={product.imgUrl} alt={product.model} boxSize="150px" objectFit="contain" />
+        <Text fontWeight="bold">
+          {product.brand} {product.model}
+        </Text>
+        <Text color="teal.500" fontSize="lg">
+          ${product.price}
+        </Text>
+      </VStack>
     </Box>
   );
 };
