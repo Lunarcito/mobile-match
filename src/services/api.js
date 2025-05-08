e872730ae9a3fba0be84db82bd6a3ba821912af4
Home <calle.carolina@gmail.com>
2025-05-08 08:48:39 +0200

    refactor(api): clean up and organize endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -7,12 +7,12 @@ export const fetchProducts = async () => {
 };
 
 export const fetchProductById = async (id) => {
-  const res = await fetch(`/api/product/${id}`);
+  const res = await fetch(`https://itx-frontend-test.onrender.com/api/product/${id}`);
   return res.json();
 };
 
 export const addToCart = async ({ id, colorCode, storageCode }) => {
-  const res = await fetch("/api/cart", {
+  const res = await fetch("https://itx-frontend-test.onrender.com/api/product/api/cart", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ id, colorCode, storageCode })
