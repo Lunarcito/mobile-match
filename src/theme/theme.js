1d89d13aa4689a0e134e8b4d445543dd6e977eb7
Home <calle.carolina@gmail.com>
2025-05-08 08:49:43 +0200

    refactor(theme): replace createSystem with extendTheme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,13 +1,9 @@
-import { createSystem, defaultBaseConfig, defineConfig } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-const customConfig = defineConfig({
-  theme: {
-    colors: {
-      brand: {
-        500: "tomato"
-      }
+export const system = extendTheme({
+  colors: {
+    brand: {
+      500: "tomato"
     }
   }
 });
-
-export const system = createSystem(defaultBaseConfig, customConfig);
