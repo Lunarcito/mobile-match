7d979fbc80fa363b4e51a766e053731bc68fb08c
Home <calle.carolina@gmail.com>
2025-05-09 18:46:48 +0200

    feat(api): implement caching for product fetching and product details

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,57 @@
 export const fetchProducts = async () => {
-  const response = await fetch("https://itx-frontend-test.onrender.com/api/product");
-  if (!response.ok) {
-    throw new Error("Error in network response");
+  const cacheKey = "productsCache";
+  const cacheExpirationKey = "productsCacheExpiration";
+  const cacheTTL = 60 * 60 * 1000;
+
+  const cachedData = localStorage.getItem(cacheKey);
+  const cachedExpiration = localStorage.getItem(cacheExpirationKey);
+
+  if (cachedData && cachedExpiration && Date.now() < Number(cachedExpiration)) {
+    return JSON.parse(cachedData);
   }
-  return await response.json();
+
+  try {
+    const response = await fetch("https://itx-frontend-test.onrender.com/api/product");
+    if (!response.ok) {
+      throw new Error("Network response error");
+    }
+    const data = await response.json();
+
+    localStorage.setItem(cacheKey, JSON.stringify(data));
+    localStorage.setItem(cacheExpirationKey, (Date.now() + cacheTTL).toString());
+    return data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
 };
 
 export const fetchProductById = async (id) => {
-  const res = await fetch(`https://itx-frontend-test.onrender.com/api/product/${id}`);
-  return res.json();
+  const cacheKey = `product_${id}`;
+  const cacheExpirationKey = `product_${id}_expiration`;
+  const cacheTTL = 60 * 60 * 1000;
+
+  const cachedData = localStorage.getItem(cacheKey);
+  const cachedExpiration = localStorage.getItem(cacheExpirationKey);
+
+  if (cachedData && cachedExpiration && Date.now() < Number(cachedExpiration)) {
+    return JSON.parse(cachedData);
+  }
+
+  try {
+    const response = await fetch(`https://itx-frontend-test.onrender.com/api/product/${id}`);
+    if (!response.ok) {
+      throw new Error("Network response error");
+    }
+    const data = await response.json();
+
+    localStorage.setItem(cacheKey, JSON.stringify(data));
+    localStorage.setItem(cacheExpirationKey, (Date.now() + cacheTTL).toString());
+    return data;
+  } catch (error) {
+    console.error(`Error fetching products details ${id}:`, error);
+    throw error;
+  }
 };
 
 export const addToCart = async ({ id, colorCode, storageCode }) => {
